2ff5a4ffc4d06840823e007d1e0346fafaa58097
yvonnebongers <yvonnebongers@gmail.com>
2026-03-15 17:01:26 +0100

    sipenspark afbeelding toegevoegd in bestand Homepage jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import {Routes, Route} from "react-router-dom";
-import Homepage from './pages/Homepage/Homepage.jsx'
+import Homepage from './pages/homepage/Homepage.jsx'
 
 function App() {
 
