a327c55c9ffba655e43f14bcbd1e229545412082
yvonnebongers <yvonnebongers@gmail.com>
2026-03-15 17:01:26 +0100

    sipenspark afbeelding toegevoegd in bestand Homepage jsx

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import sipenspark from '../../assets/sipenspark.PNG'
+
+
+function Homepage () {
+    return (
+
+        <img src={sipenspark} alt="sipenspark"/>
+  )
+}
+
+
+
+export default Homepage
