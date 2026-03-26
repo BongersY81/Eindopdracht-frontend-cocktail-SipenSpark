9ba475c88dd58eda9737866cf3238b90418dc712
yvonnebongers <yvonnebongers@gmail.com>
2026-03-26 11:37:23 +0100

    bestanden hompepage.jsx en homepage.css en bestanden app.jsx en app.css voorzien van code

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom";
 import Homepage from './pages/homepage/Homepage.jsx'
-
+import Header from './components/header/Header.jsx';
+import Footer from './components/footer/Footer.jsx';
 function App() {
 
 
   return (
     <>
+        <Header/>
         {/*toekomstige menubalk...*/}
       <Routes>
 <Route path="/" element={<Homepage/>}/>
       </Routes>
+        <Footer/>
     </>
   )
 }
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,13 +1,19 @@
-import sipenspark from '../../assets/sipenspark.PNG'
+import sipenspark from '../../assets/sipenspark.PNG';
+import './Homepage.css'
+import Navigation from "../../components/navigation/Navigation.jsx";
 
 
 function Homepage () {
     return (
-
+        <>
+<Navigation/>
+        <section className="image-container">
         <img src={sipenspark} alt="sipenspark"/>
+        </section>
+            </>
   )
 }
 
 
 
-export default Homepage
+export default Homepage;
