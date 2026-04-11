e52e0d73324b68fddffc86c11d7376a54c5c6cd6
yvonnebongers <yvonnebongers@gmail.com>
2026-04-11 18:40:50 +0200

    code geschreven voor het weergeven van invoerveld en kader zowel in search-by-name jsx als css bestand

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,14 +1,18 @@
-import './Navigation.css';
+import  './Navigation.css';
+
 
 
 function Navigation() {
     return (
         <>
             <header className="navbar_container">
-                <button>Registreren/Inloggen</button>
+
+                        <button>Registreren/Inloggen</button>
                 <button>Zoeken Bij Naam</button>
                 <button>Favorieten</button>
                 <button>Cocktail Builder</button>
+
+
             </header>
 
         </>
